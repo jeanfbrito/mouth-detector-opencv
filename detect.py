108019c62082fc2feec1ad9cae64e130f7688915
Jean Brito <jeanfbrito@gmail.com>
2020-04-25 23:28:49 -0300

    detect open mouth with minimum frames couting

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@
 #Import required modules
 import cv2
 import dlib
+import math
 
 #Dlib positions
 #  ("mouth", (48, 68)),
@@ -17,13 +18,40 @@ import dlib
 #Set up some required objects
 video_capture = cv2.VideoCapture(0) #Webcam object
 #Change Frame Rate
-video_capture.set(cv2.CAP_PROP_FPS, 30)
+video_capture.set(cv2.CAP_PROP_FPS, 10)
 #Change Resolution
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320);
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 130);
 detector = dlib.get_frontal_face_detector() #Face detector
 #Landmark identifier. Set the filename to whatever you named the downloaded file
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+def is_mouth_open(face_landmarks):
+    top_lip_top = face_landmarks.part(51)
+    top_lip_bottom = face_landmarks.part(62)
+    bottom_lip_top = face_landmarks.part(66)
+    bottom_lip_bottom = face_landmarks.part(57)
+
+    top_lip_height = math.sqrt( (top_lip_top.x - top_lip_bottom.x)**2 +
+                              (top_lip_top.y - top_lip_bottom.y)**2   )
+
+    bottom_lip_height = math.sqrt( (bottom_lip_top.x - bottom_lip_bottom.x)**2 +
+                              (bottom_lip_top.y - bottom_lip_bottom.y)**2   )
+
+    mouth_height = math.sqrt( (top_lip_bottom.x - bottom_lip_top.x)**2 +
+                              (top_lip_bottom.y - bottom_lip_top.y)**2   )
+
+    # if mouth is open more than lip height * ratio, return true.
+    ratio = 2.0
+    #print('top_lip_height: %.2f, bottom_lip_height: %.2f, mouth_height: %.2f, min*ratio: %.2f'
+         # % (top_lip_height,bottom_lip_height,mouth_height, min(top_lip_height, bottom_lip_height) * ratio))
+
+    if mouth_height > min(top_lip_height, bottom_lip_height) * ratio:
+        return True
+    else:
+        return False
+
+count_detected_frames = 0
 
 while True:
     ret, frame = video_capture.read()
@@ -36,12 +64,26 @@ while True:
 
     for k,d in enumerate(detections): #For each detected face
         shape = predictor(clahe_image, d) #Get coordinates
-        for i in range(1,68): #There are 68 landmark points on each face
+        for i in range(48,68): #There are 68 landmark points on each face
             cv2.circle(frame, (shape.part(i).x, shape.part(i).y), 1, (0,255,0), thickness=-1) #For each point, draw a red circle with thickness2 on the original frame
             #cv2.putText(frame, str(i), (shape.part(i).x,shape.part(i).y),
             #        fontFace=cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,
             #        fontScale=0.3,
             #        color=(0, 0, 255))
+        # Display text for mouth open / close
+        ret_mouth_open = is_mouth_open(shape)
+        if ret_mouth_open is True:
+            count_detected_frames = 0
+        else:
+            count_detected_frames = count_detected_frames + 1
+
+        if count_detected_frames > 15:
+            text = 'Abra a boca'
+        else:
+            text = 'Boca aberta'
+        cv2.putText(frame, text, (200, 420), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 0, 255), 1)
+
+    cv2.circle(frame,(340, 200), 50, (0,0,255), 2)
     cv2.imshow("image", frame) #Display the frame
 
     if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
